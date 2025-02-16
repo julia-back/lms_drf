f31f3bafa5affb785241756011dca52f6efedfb3
Julia Bengert <yubengert@gmail.com>
2025-02-16 20:07:02 +0300

    add payment_set in CustomUserSerializer

--- config/settings.py
+++ config/settings.py
@@ -25,10 +25,6 @@ INSTALLED_APPS = [
     "materials",
 ]
 
-REST_FRAMEWORK = {
-    "DEFAULT_FILTER_BACKENDS": "django_filters.rest_framework.DjangoFilterBackend"
-}
-
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
--- users/serializers.py
+++ users/serializers.py
@@ -2,13 +2,16 @@ from .models import CustomUser, Payment
 from rest_framework import serializers
 
 
-class CustomUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ["first_name", "last_name", "country", "phone_number", "avatar"]
-
-
 class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
         fields = "__all__"
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+
+    payment_set = PaymentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ["first_name", "last_name", "country", "phone_number", "avatar", "payment_set"]

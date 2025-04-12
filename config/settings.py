1fdfae25e3966c2678c73002985086c93d265ec2
Julia Bengert <yubengert@gmail.com>
2025-04-12 19:12:38 +0300

    fix ci.yml

--- config/settings.py
+++ config/settings.py
@@ -146,3 +146,11 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 CORS_ALLOWED_ORIGINS = ["http://localhost", "http://localhost:8000"]
 CSRF_TRUSTED_ORIGINS = ["http://localhost", "http://localhost:8000"]
 CORS_ALLOW_ALL_ORIGINS = False
+
+if "test" in sys.argv:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
